5c59bc9a803410f4493f7403b8f4a027ed128dc7
Saida DiPeri <sdiperi@Saidas-MBP.cable.rcn.com>
2020-01-18 20:55:55 -0500

    styled footer and lucid logo component

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import Quiz from "./components/Quiz";
+import Footer from "./components/Footer";
 
 export const App = () => {
   return (
@@ -8,7 +9,9 @@ export const App = () => {
     >
       <h1>Lucid</h1>
       <h2>Welcome to UI Team code assessment!</h2>
+
       <Quiz />
+      <Footer />
     </div>
   );
 };
---
+++ src/client/components/Footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+import styled from "styled-components";
+import LucidLogo from "./LucidLogo";
+
+const ScFooter = styled.footer`
+  background-color: black;
+  width: 100%;
+
+  padding-top: 20px;
+  padding-bottom: 60px;
+  display: flex;
+  justify-content: center;
+`;
+
+export default function Footer() {
+  return (
+    <ScFooter>
+      <LucidLogo width="120px" />
+    </ScFooter>
+  );
+}
