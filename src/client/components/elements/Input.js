278328af2817bd9e237628e90d69add3faed5384
sdiperi17 <sdiperi17@gmail.com>
2020-01-21 10:06:18 -0500

    added border color to input

--- src/client/components/elements/Input.js
+++ src/client/components/elements/Input.js
@@ -1,18 +1,18 @@
 import React from "react";
 import styled from "styled-components";
 
-const ScContainer = styled.div`
-  background-color: "red";
-`;
+
 
 const ScInput = styled.input`
+  border: 1px solid #dddddd;
   margin: 0.4rem;
+  padding: 5px;
 `;
 
 export default function TextInput({ name, value, onChange }) {
   return (
-    <ScContainer>
-      <ScInput onChange={onChange} type="text" name={name} value={value} />
-    </ScContainer>
+    <div>
+      <ScInput onChange={onChange} type="text" name={name} value={value} placeholder="Enter answer here" />
+    </div>
   );
 }
